8f880dbb2e0105d61ae1aa053bced5e3499be82f
nannany <ymym1990ymym@gmail.com>
2024-10-14 15:49:37 +0900

    discovery epを求められるように

--- rp/client/login_handler.go
+++
@@ -1,8 +0,0 @@
-package client
-
-import "net/http"
-
-func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	// レスポンスとしてhello worldを返す
-	w.Write([]byte("hello world"))
-}
--- rp/main.go
+++ rp/main.go
@@ -1,19 +1,96 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+	"github.com/zitadel/logging"
+	"github.com/zitadel/oidc/v3/pkg/client/rp"
+	httphelper "github.com/zitadel/oidc/v3/pkg/http"
+	"github.com/zitadel/oidc/v3/pkg/oidc"
 	"html/template"
 	"log"
+	"log/slog"
 	"net/http"
-	"rp/client"
+	"os"
+	"time"
 )
 
 var tmpl = template.Must(template.ParseFiles("templates/index.html"))
+
+var (
+	callbackPath = "/auth/callback"
+	key          = []byte("test1234test1234")
+)
 
 // ポートが8081で動くサーバーを起動する
 func main() {
 	// ハンドラを設定
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/login", client.LoginHandler)
+
+	clientID := "web"
+	clientSecret := "secret"
+	issuer := "http://localhost:8080"
+	port := "8081"
+	scopes := []string{oidc.ScopeOpenID, oidc.ScopeProfile, oidc.ScopeEmail}
+	responseMode := "query"
+
+	redirectURI := fmt.Sprintf("http://localhost:%v%v", port, callbackPath)
+	cookieHandler := httphelper.NewCookieHandler(key, key, httphelper.WithUnsecure())
+
+	logger := slog.New(
+		slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+			AddSource: true,
+			Level:     slog.LevelDebug,
+		}),
+	)
+	client := &http.Client{
+		Timeout: time.Minute,
+	}
+	// enable outgoing request logging
+	logging.EnableHTTPClient(client,
+		logging.WithClientGroup("client"),
+	)
+
+	options := []rp.Option{
+		rp.WithCookieHandler(cookieHandler),
+		rp.WithVerifierOpts(rp.WithIssuedAtOffset(5 * time.Second)),
+		rp.WithHTTPClient(client),
+		rp.WithLogger(logger),
+		rp.WithSigningAlgsFromDiscovery(),
+	}
+
+	// One can add a logger to the context,
+	// pre-defining log attributes as required.
+	ctx := logging.ToContext(context.TODO(), logger)
+	provider, err := rp.NewRelyingPartyOIDC(ctx, issuer, clientID, clientSecret, redirectURI, scopes, options...)
+	if err != nil {
+		logrus.Fatalf("error creating provider %s", err.Error())
+	}
+
+	// generate some state (representing the state of the user in your application,
+	// e.g. the page where he was before sending him to login
+	state := func() string {
+		return uuid.New().String()
+	}
+
+	urlOptions := []rp.URLParamOpt{
+		rp.WithPromptURLParam("Welcome back!"),
+	}
+
+	urlOptions = append(urlOptions, rp.WithResponseModeURLParam(oidc.ResponseMode(responseMode)))
+
+	// register the AuthURLHandler at your preferred path.
+	// the AuthURLHandler creates the auth request and redirects the user to the auth server.
+	// including state handling with secure cookie and the possibility to use PKCE.
+	// Prompts can optionally be set to inform the server of
+	// any messages that need to be prompted back to the user.
+	http.Handle("/login", rp.AuthURLHandler(
+		state,
+		provider,
+		urlOptions...,
+	))
 
 	// 8081ポートでサーバーを起動
 	log.Println("Server started at http://localhost:8081")
